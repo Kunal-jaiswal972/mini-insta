521a480252c11385e5860f1e3ae64f0b017c272f
Kunal-jaiswal972 <103945041+Kunal-jaiswal972@users.noreply.github.com>
2022-04-18 23:56:21 +0530

    Add files via upload

---
+++ CYBERLABS/js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAdGVnJ6XY1AXsO70y9-vAGKk-wulVGtt0",
+    authDomain: "mini-insta-6bd94.firebaseapp.com",
+    databaseURL: "https://mini-insta-6bd94-default-rtdb.firebaseio.com",
+    projectId: "mini-insta-6bd94",
+    storageBucket: "mini-insta-6bd94.appspot.com",
+    messagingSenderId: "762254913113",
+    appId: "1:762254913113:web:8c218103cfa28e08ce8b48",
+    measurementId: "G-WC7ESBEGYP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
